7d25688a14c127bcf367b23ea0fddae2f7894213
shenweiyan <weiyanshum@gmail.com>
2023-11-23 14:31:41 +0800

    MathJax v0.1

--- src/html.tsx
+++ src/html.tsx
@@ -50,6 +50,8 @@ export default function HTML(props: HTMLProps) {
 })()`,
           }}
         />
+	<script src="https://polyfill.io/v3/polyfill.min.js?features=es6"></script>
+	<script id="MathJax-script" async src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
